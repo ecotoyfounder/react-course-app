3523a043b37f06b09019b7e410f7ffa52eb6865c
Pavel <M3modelist@gmail.com>
2022-08-18 18:55:44 +0300

    filtering homework

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -8,23 +8,42 @@ const GroupList = ({
   onItemSelect,
   selectedItem,
 }) => {
-  console.log(Object.keys(items));
-  return (
-    <ul className="list-group">
-      {Object.keys(items).map((item) => (
-        <li
-          key={items[item][valueProperty]}
-          className={
-            "list-group-item" + (items[item] === selectedItem ? " active" : "")
-          }
-          onClick={() => onItemSelect(items[item])}
-          role="button"
-        >
-          {items[item][contentProperty]}
-        </li>
-      ))}
-    </ul>
-  );
+  if (Array.isArray(items)) {
+    return (
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            key={item[valueProperty]}
+            className={
+              "list-group-item" + (item === selectedItem ? " active" : "")
+            }
+            onClick={() => onItemSelect(item)}
+            role="button"
+          >
+            {item[contentProperty]}
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <ul className="list-group">
+        {Object.keys(items).map((item) => (
+          <li
+            key={items[item][valueProperty]}
+            className={
+              "list-group-item" +
+              (items[item] === selectedItem ? " active" : "")
+            }
+            onClick={() => onItemSelect(items[item])}
+            role="button"
+          >
+            {items[item][contentProperty]}
+          </li>
+        ))}
+      </ul>
+    );
+  }
 };
 
 GroupList.defaultProps = {
@@ -33,7 +52,7 @@ GroupList.defaultProps = {
 };
 
 GroupList.propTypes = {
-  items: PropTypes.object.isRequired,
+  items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
   valueProperty: PropTypes.string.isRequired,
   contentProperty: PropTypes.string.isRequired,
   onItemSelect: PropTypes.func,
