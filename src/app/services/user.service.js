66fa1477b7b4397a2355e97b28680e056e3214e3
Pavel <M3modelist@gmail.com>
2022-11-10 19:38:53 +0300

    adding register authentification

--- src/app/services/user.service.js
+++ src/app/services/user.service.js
@@ -3,9 +3,13 @@ import httpService from "./http.service";
 const userEndpoint = "user/";
 
 const userService = {
-    get: async () => {
-        const { data } = await httpService.get(userEndpoint);
-        return data;
-    }
+  get: async () => {
+    const { data } = await httpService.get(userEndpoint);
+    return data;
+  },
+  create: async (payload) => {
+    const { data } = await httpService.put(userEndpoint + payload._id, payload);
+    return data;
+  },
 };
 export default userService;
