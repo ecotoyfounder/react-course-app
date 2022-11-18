edf40ed50fd8df3e0194d74c1a265a08bf7751fb
Pavel <M3modelist@gmail.com>
2022-11-18 21:03:29 +0300

    add comments

--- src/app/components/ui/comments.jsx
+++ src/app/components/ui/comments.jsx
@@ -1,57 +1,66 @@
 import { orderBy } from "lodash";
 import React, { useEffect } from "react";
 import CommentsList, { AddCommentForm } from "../common/comments";
-import { useComments } from "../../hooks/useComments";
 import { useDispatch, useSelector } from "react-redux";
 import {
-    getComments,
-    getCommentsLoadingStatus,
-    loadCommentsList
+  createComment,
+  getComments,
+  getCommentsLoadingStatus,
+  loadCommentsList,
+  removeComment,
 } from "../../store/comments";
 import { useParams } from "react-router-dom";
+import { nanoid } from "nanoid";
 
 const Comments = () => {
-    const { userId } = useParams();
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(loadCommentsList(userId));
-    }, [userId]);
-    const isLoading = useSelector(getCommentsLoadingStatus());
-    const { createComment, removeComment } = useComments();
-    const comments = useSelector(getComments());
+  const { userId } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadCommentsList(userId));
+  }, [userId]);
+  const isLoading = useSelector(getCommentsLoadingStatus());
+  const comments = useSelector(getComments());
 
-    const handleSubmit = (data) => {
-        createComment(data);
+  const handleSubmit = (data) => {
+    const comment = {
+      ...data,
+      _id: nanoid(),
+      pageId: userId,
+      created_at: Date.now(),
+      userId: userId,
     };
-    const handleRemoveComment = (id) => {
-        removeComment(id);
-    };
-    const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
-    return (
-        <>
-            <div className="card mb-2">
-                <div className="card-body ">
-                    <AddCommentForm onSubmit={handleSubmit} />
-                </div>
-            </div>
-            {sortedComments.length > 0 && (
-                <div className="card mb-3">
-                    <div className="card-body ">
-                        <h2>Comments</h2>
-                        <hr />
-                        {!isLoading ? (
-                            <CommentsList
-                                comments={sortedComments}
-                                onRemove={handleRemoveComment}
-                            />
-                        ) : (
-                            "Loading..."
-                        )}
-                    </div>
-                </div>
+
+    dispatch(createComment(comment));
+  };
+  const handleRemoveComment = (id) => {
+    dispatch(removeComment(id));
+  };
+  const sortedComments = orderBy(comments, ["created_at"], ["desc"]);
+  return (
+    <>
+      <div className="card mb-2">
+        <div className="card-body ">
+          <AddCommentForm onSubmit={handleSubmit} />
+        </div>
+      </div>
+      {sortedComments.length > 0 && (
+        <div className="card mb-3">
+          <div className="card-body ">
+            <h2>Comments</h2>
+            <hr />
+            {!isLoading ? (
+              <CommentsList
+                comments={sortedComments}
+                onRemove={handleRemoveComment}
+              />
+            ) : (
+              "Loading..."
             )}
-        </>
-    );
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Comments;
